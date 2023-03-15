4b19bea8acd99cf277db12f6200a1949ba250ef9
Sone Gillis <gillis@ibleducation.com>
2023-03-15 15:52:42 +0100

    Update the admin site header to use 'Ecommerce Administration' as default

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -117,3 +117,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
+
+ADMIN_SITE_HEADER = "Ecommerce Administration"
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+admin.site.site_header = settings.ADMIN_SITE_HEADER
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
