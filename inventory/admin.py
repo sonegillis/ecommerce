c98b5764bd9c319434cafda8e29c27737ae13e28
Sone Gillis <gillis@ibleducation.com>
2023-03-19 00:32:52 +0100

    Add offer to installed_apps

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from django.template.loader import get_template
 
 from inventory.models import Product, Category, SupportedCurrency, ProductVariation, \
     ProductFeature, ProductVariationImage, Tag
-
-
-@admin.register(ProductVariationImage)
-class ProductVariationImageAdmin(admin.ModelAdmin):
-    pass
 
 
 class ProductVariationImageInline(admin.StackedInline):
@@ -56,11 +50,6 @@ class ProductFeatureAdmin(admin.ModelAdmin):
     def get_inline_instances(self, request, obj=None):
         yield from ((inline, vars(inline).update(**{"modeladmin": self, "request": request}))[0] for inline in
                     super().get_inline_instances(request, obj))
-
-
-# @admin.register(ProductVariationDeviceImage)
-# class ProductVariationDeviceImageAdmin(admin.ModelAdmin):
-#     inlines = [ProductVariationImageInline]
 
 
 @admin.register(SupportedCurrency)
